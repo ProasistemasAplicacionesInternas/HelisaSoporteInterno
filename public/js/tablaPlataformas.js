dc457b0acecfb7f477d921d04e86f15656258f96
brayan.londono <aplicacionesinternas4@helisa.com>
2023-01-24 14:36:02 -0500

    RQ_1199-ACT5-VPA

---
+++ public/js/tablaPlataformas.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $('#tabla').DataTable({
+            "paging": true,
+            
+            language: {
+                "emptyTable": "No hay información",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ Entradas",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Entradas",
+                "infoFiltered": "(Filtrado de _MAX_ total entradas)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar 10 Entradas",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscar:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                },
+            },
+     "bDestroy": true,
+        }
+
+    );
+});
+
+/*table.buttons().container()
+    .appendTo( $('.col-sm-6:eq(0)', table.table().container() ) );*/
