3335ec34afbb34550c0ed78eb66c4d1dd2013bf4
deyvid.prada <aplicacionesinternas2@helisa.com>
2023-05-30 10:39:06 -0500

    cambios en vista - adición de campo de petición y de filtro - RQ_1764

--- app/view/filtro_iso.php
+++ app/view/filtro_iso.php
@@ -1,16 +1,16 @@
 <?php
-            ini_set("session.cookie_lifetime","18000");
-            ini_set("session.gc_maxlifetime","18000");
+ini_set("session.cookie_lifetime", "18000");
+ini_set("session.gc_maxlifetime", "18000");
 
-            session_start();
-   
-            if(!isset($_SESSION['usuario'])||empty($_SESSION['usuario'])){
-       
-                 header('location:../../login.php');
-            }
-        
-     
-    ?>
+session_start();
+
+if (!isset($_SESSION['usuario']) || empty($_SESSION['usuario'])) {
+
+    header('location:../../login.php');
+}
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,7 +21,19 @@
 </head>
 
 <body>
-    <div class="container">           
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-12">
+                <h6>Elija el criterio para la consulta de ISO</h6>
+            </div>
+        </div>
+        <div class="col-3">
+            <select class="custom-select" name="criterio" id="criterio">
+                <option value="" selected>Seleccione una opción</option>
+                <option value="1">Consultar por fecha</option>
+                <option value="2">Consultar por Peticion</option>
+            </select>
+        </div>
         <div class="row mt-3" id="selectorFecha">
             <div class="col-12">
                 <h6>Seleccione el rango de fechas para generar la consulta</h6>
@@ -36,7 +48,21 @@
                     <input type="submit" id="btn-consultarFechaI" name="btn-consultarFechaI" class="btn btn-info" value="Consultar">
                 </form>
             </div>
-        </div>     
+        </div>
+        <div class="row" id="selectorTicket">
+            <div class="col-12">
+                <h6>Número de Peticion</h6>
+            </div>
+            <div class="col-3">
+                <form action="app/view/peticiones_iso.php" target="_blank" method="post" onsubmit="rango()">
+                    <div class="form-group">
+                        <input type="hidden" id="usuario_actual" name="usuario_actual" value="<?php echo $_SESSION['usuario']; ?>">
+                        <input type="text" id="peticionFiltro" name="peticionFiltro" class="form-control" placeholder="Numero de Peticion" required autocomplete="off">
+                    </div>
+                    <input type="submit" id="btn-consultarTicketI" name="btn-consultarTicketI" class="btn btn-info" value="Consultar">
+                </form>
+            </div>
+        </div>
     </div>
     <script src="public/js/iso.js"></script>
     <script src="public/js/bloqueoTeclas.js"></script>
