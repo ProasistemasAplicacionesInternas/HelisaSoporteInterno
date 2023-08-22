de9578e8b9efa01e707ad50144ec3c3170fc37a6
AplicacionesInternas <133372960+AplicaionesInternas@users.noreply.github.com>
2023-08-22 14:06:47 -0500

    RQ_1972- IDENTIFICAR QUE USUARIOS ESTAN EN PLATAFORMA

--- app/controller/controlador_plataformas.php
+++ app/controller/controlador_plataformas.php
@@ -10,6 +10,9 @@
     }else if(isset($_POST['modificarPlataforma'])){
         $consulta = $plataforma->modificarPlataforma($_POST['id'],$_POST['administrador'],$_POST['estado']);
         echo $consulta;
+    }else if(isset($_POST['usuariosPeticiones'])){
+        $consulta = $plataforma->usuariosPeticiones($_POST['id']);
+        echo $consulta;
     }else{
         $plataformas = $plataforma->getPlataformas();
     }
--- public/js/plataformas.js
+++ public/js/plataformas.js
@@ -1,101 +1,138 @@
-$('#crear_plataforma').click(function(){
-  var plataforma = $('#modal_descripcionCrear').val();
-  var administrador = $('#modal_administradorCrear').val();
+$("#crear_plataforma").click(function () {
+  var plataforma = $("#modal_descripcionCrear").val();
+  var administrador = $("#modal_administradorCrear").val();
 
-  if(plataforma == '' || plataforma == null || plataforma == undefined){
-      $.smkAlert({
-          text: 'Debe asignarle un nombre a la plataforma.',
-          type: 'warning'
-      });
-  }else if(administrador == '' || administrador == null || administrador == undefined){
-      $.smkAlert({
-          text: 'Debe asignarle un Administrador a la plataforma.',
-          type: 'warning'
-      });
-  }else{
-      data = "crearPlataforma=1" +
-      "&descripcion=" + plataforma +
-      "&administrador=" + administrador;
-      $.ajax({
-          type: 'POST',
-          url: 'app/controller/controlador_plataformas.php',
-          data: data,   
-      }).done(function(data){
-          if(data == 1){
-              $('#modal_descripcionCrear').val("");
-              $('.close').click();
-              $("#contenido").load("app/view/plataformas.php");
-          }else if(data == 2){
-              $.smkAlert({
-                  text: 'El titulo asignado ya se encuentra resgirtado, debera designarle otro.',
-                  type: 'warning'
-              });
-          }else{
-              $('#modal_descripcionCrear').val("");
-              $('.close').click();
-              $.smkAlert({
-                  text: 'Erorr Interno, intentelo nuevamente.',
-                  type: 'danger'
-              });           
-          }
-      }) 
+  if (plataforma == "" || plataforma == null || plataforma == undefined) {
+    $.smkAlert({
+      text: "Debe asignarle un nombre a la plataforma.",
+      type: "warning",
+    });
+  } else if (
+    administrador == "" ||
+    administrador == null ||
+    administrador == undefined
+  ) {
+    $.smkAlert({
+      text: "Debe asignarle un Administrador a la plataforma.",
+      type: "warning",
+    });
+  } else {
+    data =
+      "crearPlataforma=1" +
+      "&descripcion=" +
+      plataforma +
+      "&administrador=" +
+      administrador;
+    $.ajax({
+      type: "POST",
+      url: "app/controller/controlador_plataformas.php",
+      data: data,
+    }).done(function (data) {
+      if (data == 1) {
+        $("#modal_descripcionCrear").val("");
+        $(".close").click();
+        $("#contenido").load("app/view/plataformas.php");
+      } else if (data == 2) {
+        $.smkAlert({
+          text: "El titulo asignado ya se encuentra resgirtado, se debe designar otro.",
+          type: "warning",
+        });
+      } else {
+        $("#modal_descripcionCrear").val("");
+        $(".close").click();
+        $.smkAlert({
+          text: "Erorr interno, intentelo nuevamente.",
+          type: "danger",
+        });
+      }
+    });
   }
-})
+});
 
-function modalModificar(id,descripcion,administrador,estado){
-  $('#modal_id').val(id);
-  $('#modal_descripcion').val(descripcion);
+function modalModificar(id, descripcion, administrador, estado) {
+  $("#modal_id").val(id);
+  $("#modal_descripcion").val(descripcion);
   $("#modal_administrador").val(administrador);
   $("#modal_estado").val(estado);
 }
 
-
-$('#modificar_plataforma').click(function(){
-  var id = $('#modal_id').val();
-  var plataforma = $('#modal_descripcion').val();
-  var administrador = $('#modal_administrador').val();
+$("#modificar_plataforma").click(function () {
+  var id = $("#modal_id").val();
+  var plataforma = $("#modal_descripcion").val();
+  var administrador = $("#modal_administrador").val();
   var estado = $("#modal_estado").val();
 
-  if(plataforma == '' || plataforma == null || plataforma == undefined){
-      $.smkAlert({
-          text: 'Debe asignarle un nombre a la plataforma.',
-          type: 'warning'
-      });
-  }else if(administrador == '' || administrador == null || administrador == undefined){
-      $.smkAlert({
-          text: 'Debe asignarle un Administrador a la plataforma.',
-          type: 'warning'
-      });
-  }else{
-      data = "modificarPlataforma=1" +
-      "&id=" + id +
-      "&descripcion=" + plataforma +
-      "&administrador=" + administrador +
-      "&estado=" + estado;
-      $.ajax({
-          type: 'POST',
-          url: 'app/controller/controlador_plataformas.php',
-          data: data,   
-      }).done(function(data){
-          if(data == 1){
-              $('.close').click();
-              $("#contenido").load("app/view/plataformas.php");
-          }else if(data == 6000){
-              $.smkAlert({
-                  text: 'Para poder inactivar esta plataforma, dichas peticiones deberan finalizar',
-                  type: 'warning'
+  if (plataforma == "" || plataforma == null || plataforma == undefined) {
+    $.smkAlert({
+      text: "Debe asignarle un nombre a la plataforma.",
+      type: "warning",
+    });
+  } else if (
+    administrador == "" ||
+    administrador == null ||
+    administrador == undefined
+  ) {
+    $.smkAlert({
+      text: "Debe asignarle un Administrador a la plataforma.",
+      type: "warning",
+    });
+  } else {
+    data =
+      "modificarPlataforma=1" +
+      "&id=" +
+      id +
+      "&descripcion=" +
+      plataforma +
+      "&administrador=" +
+      administrador +
+      "&estado=" +
+      estado;
+    $.ajax({
+      type: "POST",
+      url: "app/controller/controlador_plataformas.php",
+      data: data,
+    }).done(function (data) {
+      if (data == 1) {
+        $(".close").click();
+        $("#contenido").load("app/view/plataformas.php");
+      } else if (data == 6000) {
+        $.smkAlert({
+          text: "No se puede inactivar la plataforma (" + plataforma + ") porque cuenta con una petición nueva",
+          type: "warning",
+        });
+      } else if (data == 7000) {
+        data =
+          "usuariosPeticiones=1" +
+          "&id=" +
+          id;
+        $.ajax({
+          type: "POST",
+          url: "app/controller/controlador_plataformas.php",
+          data: data,
+        }).done(function (data) {
+          if (data.lenght !== 0) {
+            $.smkAlert({
+                text: "No se puede inactivar la plataforma (" + plataforma + ") porque tiene peticiones de acceso activas por: \n" + data,
+                type: "danger",
               });
-              $.smkAlert({
-                  text: 'No se puede Inactivar esta plataforma ya que esta asociada a varias peticions de Accesoo',
-                  type: 'warning'
-              });          
-          }else{
-              $('.close').click();
-              $.smkAlert({
-                  text: 'Erorr Interno, intentelo nuevamente.',
-                  type: 'danger'
-              });           
+         
+            } else {
+            $(".close").click();
+            $.smkAlert({
+              text: "Error al traer los usuarios de las peticiones asociadas.",
+              type: "danger",
+            });
           }
-      }) 
+        });
+
+     
+    } else {
+        $(".close").click();
+        $.smkAlert({
+          text: "Error interno, intentelo nuevamente.",
+          type: "danger",
+        });
+      }
+    });
   }
-})
+});
